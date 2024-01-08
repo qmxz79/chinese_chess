33f17d9830fe1eeeb3899635f19ccc4d76822b76
shirne <shirne@126.com>
2024-01-08 11:45:48 +0800

    fix duplicate logger listener

--- plugins/engine_interface/lib/src/logger.dart
+++ plugins/engine_interface/lib/src/logger.dart
@@ -2,14 +2,23 @@ import 'dart:developer';
 
 import 'package:logging/logging.dart';
 
-final logger = Logger('ENGINE')
-  ..onRecord.listen((record) {
-    log(
-      record.message,
-      time: record.time,
-      level: record.level.value,
-      error: record.error,
-      stackTrace: record.stackTrace,
-      sequenceNumber: record.sequenceNumber,
-    );
-  });
+final logger = _getLogger('ENGINE');
+
+bool _loggerListened = false;
+Logger _getLogger(String name) {
+  final logger = Logger('CCHESS');
+  if (!_loggerListened && hierarchicalLoggingEnabled) {
+    _loggerListened = true;
+    logger.onRecord.listen((record) {
+      log(
+        record.message,
+        time: record.time,
+        level: record.level.value,
+        error: record.error,
+        stackTrace: record.stackTrace,
+        sequenceNumber: record.sequenceNumber,
+      );
+    });
+  }
+  return logger;
+}
